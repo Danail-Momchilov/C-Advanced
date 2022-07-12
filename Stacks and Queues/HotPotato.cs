171eeecf08f83f706732086d9f50baa66f2b3229
Danail Momchilov <46314846+Danail-Momchilov@users.noreply.github.com>
2022-07-12 14:51:47 +0300

    Create HotPotato.cs

---
+++ Stacks and Queues/HotPotato.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+class HotPotato
+{
+    static void Main()
+    {
+        var children = Console.ReadLine().Split(' ');
+        var number = int.Parse(Console.ReadLine());
+
+        Queue<string> game = new Queue<string>(children);
+        while (game.Count > 1)
+        {
+            for (int i = 0; i < game.Count; i++)
+            {
+                game.Enqueue(game.Dequeue());
+            }
+            Console.WriteLine($"Removed {game.Dequeue()}");
+        }
+        Console.WriteLine($"Last is {game.Dequeue()}");
+    }
+}
